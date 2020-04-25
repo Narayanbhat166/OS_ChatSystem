854181657c29937925e6a47bde1b1a00d473cd10
Narayan Bhat <bhatj1964@gmail.com>
2020-04-25 17:01:32 +0530

    Shared Memory Accessibe over other processes

---
+++ main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//Semaphores
+#include "semaphore_header.h"
+
+//Shared Memory
+#include "shm_header.h"
+
+void writeMessage(/*shared_mem_segment,*/ char *msg)
+{
+	// insert producer code here
+	// the process becomes the producer
+}
+char *readMessage(/*shared_mem_segment*/)
+{
+	// insert consumer code here
+	// the process becomes the consumer
+}
+
+int main(int argc, char *argv[])
+{
+	//Address of our shared memory buffer
+	char *buffer = NULL;
+	int shm_id = 0;
+
+	char User1[20], User2[20];
+
+	if (argc != 2)
+	{
+		// create shared memory segment
+		int shm_size = 1024;
+		shm_id = AllocateSharedMemory(shm_size);
+		buffer = (char *)MapSharedMemory(shm_id);
+		puts("You are User:1 Enter Your Name:");
+		scanf("%s", User1);
+		sprintf(buffer, User1);
+
+		printf("Use id: %d to get another user connected\n", shm_id);
+		while (strcmp(buffer, User1) == 0)
+			;
+
+		strcpy(User2, buffer);
+		//debugging goes down here
+	}
+	else
+	{
+		//Shared memory created by other user just map it
+		shm_id = atoi(argv[1]);
+		buffer = (char *)MapSharedMemory(shm_id);
+		puts("You are User:2 Enter your Name:");
+		scanf("%s", User2);
+		strcpy(User1, buffer);
+		sprintf(buffer, User2);
+
+		//debugging goes down here
+	}
+
+	printf("%s and %s are now connected\n", User1, User2);
+	while (1)
+	{
+	}
+
+	// if memory is not full, allow writing
+	// if memory is full, extend the memory segment
+
+	return 0;
+}
---
+++ shm_header.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <assert.h>
+
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+/** * Allocates a shared memory segment. *
+ * @param n Size (in bytes) of chunk to allocate. 
+ * @return Id of shared memory chunk. */
+
+int AllocateSharedMemory(int n)
+{
+    assert(n > 0);
+    return shmget(IPC_PRIVATE, n, IPC_CREAT | SHM_R | SHM_W);
+}
+
+/** 
+ * Maps a shared memory segment onto our address space. 
+ *  
+ *  @param id Shared memory block to map.
+ *  @return Address of mapped block. */
+
+void *MapSharedMemory(int id)
+{
+    void *addr;
+    assert(id != 0);
+    addr = shmat(id, NULL, 0);
+    shmctl(id, IPC_RMID, NULL); /*mark segment to be destroyed.When all processes are detached from the memory segment, it will be freed */
+    printf("Mapped shared memory to address %p\n", addr);
+    return addr;
+}
