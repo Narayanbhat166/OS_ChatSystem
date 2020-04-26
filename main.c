2ac5f3679dd367a4efeeb102c408f1faa8574641
Narayan Bhat <bhatj1964@gmail.com>
2020-04-26 09:55:29 +0530

    Chat history feature not working

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 //Semaphores
 #include "semaphore_header.h"
 
@@ -23,9 +24,11 @@ int main(int argc, char *argv[])
 	//Address of our shared memory buffer
 	char *buffer = NULL;
 	int shm_id = 0;
+	FILE *history = NULL;
 
 	char User1[20], User2[20];
 
+	//below code segment is for the setup
 	if (argc != 2)
 	{
 		// create shared memory segment
@@ -34,13 +37,25 @@ int main(int argc, char *argv[])
 		buffer = (char *)MapSharedMemory(shm_id);
 		puts("You are User:1 Enter Your Name:");
 		scanf("%s", User1);
-		sprintf(buffer, User1);
+		strcpy(buffer, User1);
 
 		printf("Use id: %d to get another user connected\n", shm_id);
+
+		//Wait for Another user to get connected
 		while (strcmp(buffer, User1) == 0)
 			;
 
 		strcpy(User2, buffer);
+		history = fopen("chathistory.txt", "w");
+		if (history == NULL)
+		{
+			printf("File not opened\n");
+		}
+
+		time_t now;
+		time(&now);
+		fprintf(history, "Chat History:");
+		fprintf(history, "Conversation between %s and %s\n", User1, User2);
 		//debugging goes down here
 	}
 	else
@@ -51,7 +66,10 @@ int main(int argc, char *argv[])
 		puts("You are User:2 Enter your Name:");
 		scanf("%s", User2);
 		strcpy(User1, buffer);
-		sprintf(buffer, User2);
+		strcpy(buffer, User2);
+
+		//open the file in append mode
+		//history = fopen("chathistory.txt", "a");
 
 		//debugging goes down here
 	}
@@ -63,6 +81,10 @@ int main(int argc, char *argv[])
 
 	// if memory is not full, allow writing
 	// if memory is full, extend the memory segment
+
+	//Release all the resources
+	fclose(history);
+	shmdt(buffer);
 
 	return 0;
 }
